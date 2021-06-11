2f416a83b671acc2b91032eb2fdc83236ef8035c
Myeongkook Park <myeongkuk@hotmail.com>
2021-06-11 19:51:26 +0900

    6/11 using localstorage

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+const h1 = document.querySelector("#greeting");
 
-function onLoginBtnClick(){
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+function onLoginBtnClick(event){
+    event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings();
 }
 
-loginButton.addEventListener("click",onLoginBtnClick);
+function paintGreetings(){
+    const input = localStorage.getItem(USERNAME_KEY)
+    h1.classList.remove(HIDDEN_CLASSNAME);
+    h1.innerText = `Hello ${input}`
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginBtnClick)
+    // show the form
+}else{
+    paintGreetings();
+    // show the greetings
+}
